3846169ab1604504cf7a56f607340583512b9e17
Scott Barona <vecartphoto@gmail.com>
2022-01-12 23:33:46 -0500

    initial packages installed, app rendering loading, error

---
+++ src/Components/Planets.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+//Query String grabbed from Hasura
+const PLANETS = gql`
+{
+    planets {
+      id
+      name
+      cuisine
+    }
+  }
+`;
+
+//Component
+//useQuery function call, passing in our query
+export default function Planets() {
+    const { loading, error, data } = useQuery(PLANETS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error. </p>;
+
+    return data.planets.map(({ id, name, cuisine }) => (
+        <div key={id}>
+            <p>
+                {name} | {cuisine}
+            </p>
+        </div>
+    ));
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import Planets from './Components/Planets';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://enabling-deer-37.hasura.app/v1/graphql'
+  })
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Planets />
+  </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById('root'));
